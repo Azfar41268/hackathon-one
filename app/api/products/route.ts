132715981686ff8ec75cc12d671a644f5b5700ee
Azfar41268 <azfarsikander33@gmail.com>
2023-09-10 11:00:02 +0500

    ok

--- app/api/products/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse, NextRequest } from "next/server";
-import { db, itemsTable, Item } from "@/lib/drizzle";
-import { sql } from "@vercel/postgres";
-
-export async function GET(request: NextRequest) {
-    const products = await db.select().from(itemsTable).execute();
-
-    return NextResponse.json({ products });
-}
